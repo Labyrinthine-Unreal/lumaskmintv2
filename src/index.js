7eb6f17796f00e254fa1c5b89098da6851c7a16d
calvingreen94 <calvingreen94@aol.com>
2022-07-01 22:47:02 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./pages/App";
-import land from "./pages/land";
+import App from "./App";
+// import land from "./pages/land";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
